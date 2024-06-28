5095b711b37167afebb3c17c78484a6b33e6d7eb
Aman <124717695+a-man1980@users.noreply.github.com>
2024-06-28 23:30:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let text = document.getElementById('text');
+let leaf = document.getElementById('leaf');
+let hill1 = document.getElementById('hill1');
+let hill4 = document.getElementById('hill4');
+let hill5 = document.getElementById('hill5');
+
+window.addEventListener('scroll',()=>{
+    let val=window.scrollY;
+
+    text.style.marginTop= val*2.5+'px';
+    leaf.style.left= val*1.5+'px';
+    leaf.style.top= val*-1.5+'px';
+    hill5.style.left= val*1.5+'px';
+    hill4.style.left= val*-1.5+'px';
+    hill1.style.top= val*1+'px';
+});
