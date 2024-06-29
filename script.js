6ed296d3c5f301bc28b20d7de9986cccd8c307ec
Aman <124717695+a-man1980@users.noreply.github.com>
2024-06-29 16:10:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ let hill5 = document.getElementById('hill5');
 
 window.addEventListener('scroll',()=>{
     let val=window.scrollY;
-
+    if(val>480){
+        val=0;
+    }
     text.style.marginTop= val*2.5+'px';
     leaf.style.left= val*1.5+'px';
     leaf.style.top= val*-1.5+'px';
     hill5.style.left= val*1.5+'px';
     hill4.style.left= val*-1.5+'px';
-    hill1.style.top= val*1+'px';
+    hill1.style.top= val*1.5+'px';
 });
